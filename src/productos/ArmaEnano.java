a8582cf733f2fa4120ae725ab61f6b17408440f8
Juandials <judilemo@gmail.com>
2019-09-01 21:37:53 -0500

    Upload

---
+++ src/productos/ArmaEnano.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package productos;
+
+/**
+ *
+ * @author judil
+ */
+public class ArmaEnano implements Arma{
+    @Override
+    public String Estado() {
+        return "Creando arma enano";
+    }
+}
---
+++ src/productos/CuerpoEnano.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package productos;
+
+/**
+ *
+ * @author judil
+ */
+public class CuerpoEnano implements Cuerpo{
+    @Override
+    public String Estado() {
+        return "Creando cuerpo enano";
+    }
+}
